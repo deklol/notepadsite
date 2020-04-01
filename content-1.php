1e0555a71769de41ca06767547dbfbcf3d10cb41
deklol <50808697+deklol@users.noreply.github.com>
2020-04-01 19:43:04 +0100

    hmm
    hmm

---
+++ content-1.php
@@ -0,0 +1,25 @@
+<div class="message">
+<div class="profile-icon"><img class="responsive-img" src="pfp.gif"></div>
+<div class="message-content">
+    <div class="user-name"><a href="#">dek</a> <span class="timestamp">Today at 13:37</span></div>
+<strong>ＷＥＬＣＯＭＥ　ＴＯ<br/>
+ＴＨＥ　ＧＯＯＳＥ　ＨＯＵＳＥ　ＤＩＳＣＯＲＤ　ＳＥＲＶＥＲ．</strong><br/><br/>
+
+Stick to the fuckin' rules and we're all good.<br/><br/>
+
+<strong>- Use the right fucking channels.</strong><br/>
+<strong>- No spam, ads (including self-promotion), etc.</strong><br/>
+<strong>- Don't troll in the text channels.</strong><br/>
+<strong>- Don't abuse mentions.</strong><br/>
+<strong>- Don't abuse your admin rights.</strong><br/>
+<strong>- Don't abuse the bots.</strong><br/><br/>
+
+*Do not type in this channel. Delete the current GIF before adding a new one.*
+ </div>                
+</div>
+<div class="message">
+    <div class="profile-icon"></div>
+    <div class="message-content">
+        <img src="https://cdn.discordapp.com/attachments/693496554148855895/693660698172981258/cero-miedo-gif-4.gif"/>
+     </div>                
+    </div>
---
+++ content-2.php
@@ -0,0 +1,6 @@
+<div class="message">
+    <div class="profile-icon"></div>
+    <div class="message-content">
+        <img src="https://cdn.discordapp.com/attachments/693496554148855895/693660698172981258/cero-miedo-gif-4.gif"/>
+     </div>                
+    </div>
